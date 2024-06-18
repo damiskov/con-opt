bb8bddb449c58cba39ddc58e9399fc6a97ef5460
DavidMiles-Skov <milesskovd@gmail.com>
2024-06-18 18:55:48 +0200

    clean up repo. Upload report.

--- himmelblauHessian.m
+++
@@ -1,8 +0,0 @@
-function H = himmelblauHessian(x)
-%{
-    Hessian of the objective function for our Himmelblau test problem.
-%}
-
-H = [12*x(1)^2 + 4*x(2) - 42, 4*x(1) + 4*x(2);
-    4*x(1) + 4*x(2), 4*x(1) + 12*x(2)^2 - 26];
-end
